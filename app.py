80ef95237058d78c8e40e7422d85e5d82cbb252a
Juan Pablo <A01208471@itesm.mx>
2017-03-08 20:52:53 +0100

    Agregar botones acomodados, cambiando de color individualmente

--- app.py
+++ app.py
@@ -6,25 +6,24 @@ class Application(Frame):
         #super se utiliza para obtener los metodos del padre, en este caso el frame
         super(Application, self).__init__()
         self.master = master
-        self.crear_focos(5)
-        self.grid(row=0, column=0, sticky=N + S + E + W)
+        self.crear_focos()
+        #self.grid(row=0, column=0, sticky=N + S + E + W)
 
+    def crear_focos(self):
+        for row_index in range(10):
+            Grid.rowconfigure(self, row_index, weight=1)
+            for col_index in range(10):
+                Grid.columnconfigure(self, col_index, weight=1)
+                btn = Foco()
+                #btn = Button(self)  # create a button inside frame
+                btn.grid(row=row_index, column=col_index, sticky=N + S + E + W)
 
-    #crea la cantidad de focos pasados en el parametro x, de momento no se utiliza (CAMBIAR)
+'''
+    crea la cantidad de focos pasados en el parametro x, de momento no se utiliza (CAMBIAR)
     def crear_focos(self, x):
         focos = []
         for i in range(0,25):
             focos.append(Foco())
             focos[i].pack()
 
-
-'''
-    def crear_focos(self, x):
-        focos = []
-        for row_index in range(10):
-            Grid.rowconfigure(self, row_index, weight=1)
-            for col_index in range(10):
-                Grid.columnconfigure(self, col_index, weight=1)
-                btn = Button(self)  # create a button inside frame
-                btn.grid(row=row_index, column=col_index, sticky=N + S + E + W)
 '''
--- foco.py
+++ foco.py
@@ -1,16 +1,18 @@
 from tkinter import *
 
+
 class Foco(Button):
 
     def __init__(self, *args, **kwargs):
         Button.__init__(self, *args, **kwargs, command=self.cambiacolor)
         self['bg'] = 'green'
+        #self.rowconfigure(self, row=roww, col=coll, weight=1, )
+        #self.sticky = N + S + E + W
 
     def cambiacolor(self):
         if self.cget('bg') == 'grey':
             self.configure(bg='green')
         else:
             self.configure(bg='grey')
-        self.pack()
 
 
